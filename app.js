dadc306a41ae06ad8de04f859a1c1eedd4150b3f
Walter Rumsby <walter@rumsby.net>
2015-06-15 22:05:15 +1200

    Depend on b

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 require.config({
   paths: {
     'a': 'bower_components/a/index',
+    'b': 'bower_components/b/index',
     'king-dom': '/bower_components/king-dom/index'
   }
 });
 
-require(['a'], function (a) {
-  a('#content');
+require(['a', 'b'], function (a, b) {
+  var aInterval = Math.floor(Math.random() * 1000);
+  var bInterval = Math.floor(Math.random() * 1000);
+
+  window.setInterval(function () {
+    a('#content');
+  }, aInterval);
+
+  window.setInterval(function () {
+    b('#content');
+  }, bInterval);
 });
